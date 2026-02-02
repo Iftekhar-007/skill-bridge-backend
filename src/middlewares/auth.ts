8fd9f59cdf3a61b1e02fddd0f8adc7b8d53d09a0
Iftekhar-007 <shawonsp534@gmail.com>
2026-02-02 14:53:04 +0600

    auth middleware added

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,56 @@
+import { NextFunction, Request, Response } from "express";
+import { auth } from "../lib/auth";
+import { fromNodeHeaders } from "better-auth/node";
+
+export enum UserRole {
+  admin = "ADMIN",
+  user = "USER",
+  student = "STUDENT",
+  tutor = "TUTOR",
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        id: string;
+        email: string;
+        name: string;
+        role: string;
+      };
+    }
+  }
+}
+
+const authMiddle = (...roles: UserRole[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const session = await auth.api.getSession({
+      headers: fromNodeHeaders(req.headers),
+    });
+
+    if (!session) {
+      return res.status(403).json({
+        success: false,
+        message: "You are not authorized!",
+      });
+    }
+
+    req.user = {
+      id: session.user.id,
+      email: session.user.email,
+      name: session.user.name,
+      role: session.user.role as string,
+    };
+
+    if (!roles.length && !roles.includes(req.user.role as UserRole)) {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden Access!! You are not allowed to access this route",
+      });
+    }
+
+    next();
+  };
+};
+
+export default authMiddle;
